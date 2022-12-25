e63b5360ee121b3566825b74bc2566862140a5e4
abdallamfathy <abdallamfathy@gmail.com>
2022-12-26 00:33:31 +0200

    edits in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,9 @@ const Navbar = () => {
     <>
         <nav className='bg-gray-800 '>
         <div className=' text-white flex flex-row grow-0 items-center justify-between mx-8  '>
-            <div className=''>
-            <button className='bg-gray-700 text-white rounded-xl px-4 py-2 w-36 h-10 font-semibold'>تسجيل الدخول</button>
+            <div className=' flex gap-4'>
+            <button className='bg-gray-700 text-white rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>الاشتراك</button>
+            <button className='bg-gray-700 text-white rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>تسجيل الدخول</button>
             </div>
             <div className='bg-white rounded-lg w-[900px] h-12 flex justify-between '>
             <button className='bg-yellow-500 text-white p-2  w-12 rounded-l-lg'><BiSearchAlt className='w-7 h-7'/></button>
@@ -16,7 +17,7 @@ const Navbar = () => {
             <button className='bg-gray-400 text-white pl-4 py-2 w-20 border-l border-gray-500 rounded-r-lg flex items-center gap-1 font-semibold'> <IoMdArrowDropdown/>  الكل</button>
             </div>
             <div className=''>
-            <img src={logo} alt="logo" className='w-52 h-28  ' />
+            <img src={logo} alt="logo" className='w-48 h-28 ' />
             </div>
         </div>
         </nav>
