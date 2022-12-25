abbe375155fe3d61b40ec1c0f93bf21d457cd2ae
abdallamfathy <abdallamfathy@gmail.com>
2022-12-26 00:46:17 +0200

    add use effect for loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import {Navbar , Loader} from './components'
+import {useEffect , useState} from "react"
 
 function App() {
+  const [loading , setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => setLoading(false) , 1000)
+  }, [])
+  
 
   return (
     <>
-      <Navbar/>
-      <Loader/>
+      {!loading && <Navbar/>}
+      {loading && <Loader/>}
     </>
   )
 }
