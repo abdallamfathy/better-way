0f432306c3988cd4b4725be956707657505734e5
abdallamfathy <abdallamfathy@gmail.com>
2022-12-27 22:51:09 +0200

    add mobile version for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import logo from '../assets/logo2.png'
+import React, { useState } from 'react'
+import logo from '../assets/logo.png'
 import {BiSearchAlt , IoMdArrowDropdown} from 'react-icons/all'
+import {HiMenuAlt3} from 'react-icons/hi'
 
 const Navbar = () => {
+
+  
+  const [show, setShow] = useState(false)  
+  const toggleMenu = (prev) => {
+    setShow(!show);
+    console.log(show);
+  }
+
   return (
     <>
         <nav className='bg-[#4C4C6D] '>
-        <div className=' text-[#4C4C6D] flex flex-row grow-0 items-center justify-between mx-8  '>
-            <div className=' flex gap-4'>
+        <div className='sm:hiden flex justify-between items-center mr-5'>
+        <img src={logo} alt="logo" className='w-40 h-28 ' />
+        <HiMenuAlt3 className='w-12 h-14 text-white' onClick={() => toggleMenu()}/>
+        </div>
+
+        {/* Mobile version  */}
+
+        <div className={show ? 'flex flex-col justify-center items-center bg-[#0000f0] h-screen w-10/12' : 'hidden'}>
+        <div className='flex flex-col justify-center items-center h-screen w-10'>
+        s
+        
+        </div>
+        </div>
+
+
+        <div className='sm:flex hidden flex-row grow-0 items-center justify-between mx-8 text-[#4C4C6D]  '>
+          
+          <div className='flex gap-4  '>
             <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>الاشتراك</button>
             <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>تسجيل الدخول</button>
             </div>
@@ -16,10 +41,7 @@ const Navbar = () => {
             <input type="text" placeholder='' className='bg-white text-black rounded-xl pl-4 py-2 w-96 text-right' />
             <button className='bg-[#B8DFD8]  pl-4 py-2 w-20 border-l border-[#E8F6EF] rounded-r-lg flex items-center gap-1 font-semibold'> <IoMdArrowDropdown/>  الكل</button>
             </div>
-            <div className=''>
-            <img src={logo} alt="logo" className='w-48 h-28 ' />
             </div>
-        </div>
         </nav>
     </>
   )
