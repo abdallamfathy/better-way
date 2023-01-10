859a8e149ffc12e2283f88f56d8e23137cdda242
abdallamfathy <abdallamfathy@gmail.com>
2023-01-10 23:43:48 +0200

    navbar desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <>
         <nav className='bg-bg '>
-        <div className='sm:hiden flex justify-between items-center mx-5'>
+        <div className='sm:hidden flex justify-between items-center mx-5'>
           <div className='flex justify-center items-center'>
         {!show && <AiOutlineMenu className='w-8 h-10 text-white' onClick={() => toggleMenu()}/>}
         {show && <AiOutlineClose className='w-8 h-10 text-white' onClick={() => toggleMenu()}/>}
@@ -30,21 +30,24 @@ const Navbar = () => {
 
         {/* Mobile version  */}
 
-        <div className={show ? 'flex flex-col justify-start items-start bg-bg  h-screen w-full' : 'hidden'}>
+        <div className={show ? 'flex flex-col justify-start items-start bg-bg  h-screen w-full md:hidden' : 'hidden'}>
         <Categories/>
         </div>
 
 
         <div className='sm:flex hidden flex-row grow-0 items-center justify-between mx-8 text-[#4C4C6D]  '>
           
-          <div className='flex gap-4  '>
-            <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>الاشتراك</button>
-            <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>تسجيل الدخول</button>
+            <div>
+            <img src={logo} alt="logo" className='w-28 h-24 ' />
             </div>
             <div className='bg-white rounded-lg w-[900px] h-12 flex justify-between '>
             <button className='bg-[#FFE194]  p-2  w-12 rounded-l-lg'><BiSearchAlt className='w-7 h-7'/></button>
             <input type="text" placeholder='' className='bg-white text-black rounded-xl pl-4 py-2 w-96 text-right' />
-            <button className='bg-[#B8DFD8]  pl-4 py-2 w-20 border-l border-[#E8F6EF] rounded-r-lg flex items-center gap-1 font-semibold'> <IoMdArrowDropdown/>  الكل</button>
+            <button className='bg-[#B8DFD8]  pl-1 py-2 w-20 border-l border-[#E8F6EF] rounded-r-lg flex items-center gap-1 font-semibold'> <IoMdArrowDropdown/>  Search</button>
+            </div>
+            <div className='flex gap-4  '>
+            <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>Sign Up</button>
+            <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>Log In</button>
             </div>
             </div>
         </nav>
