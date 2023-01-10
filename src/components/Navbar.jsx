ff539e35398594e699841663b9cf7f76bc0f94e7
abdallamfathy <abdallamfathy@gmail.com>
2023-01-11 00:27:50 +0200

    navbar button colors and navigation color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,13 +41,13 @@ const Navbar = () => {
             <img src={logo} alt="logo" className='w-28 h-24 ' />
             </div>
             <div className='bg-white rounded-lg w-[900px] h-12 flex justify-between '>
-            <button className='bg-[#FFE194]  p-2  w-12 rounded-l-lg'><BiSearchAlt className='w-7 h-7'/></button>
+            <button className='bg-btn  p-2  w-12 rounded-l-lg'><BiSearchAlt className='w-7 h-7 text-white'/></button>
             <input type="text" placeholder='' className='bg-white text-black rounded-xl pl-4 py-2 w-96 text-right' />
-            <button className='bg-[#B8DFD8]  pl-1 py-2 w-20 border-l border-[#E8F6EF] rounded-r-lg flex items-center gap-1 font-semibold'> <IoMdArrowDropdown/>  Search</button>
+            <button className='bg-gray-200  pl-1 py-2 w-20 border-l border-[#E8F6EF] rounded-r-lg flex items-center gap-1 font-semibold'> <IoMdArrowDropdown/>  Search</button>
             </div>
             <div className='flex gap-4  '>
-            <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>Sign Up</button>
-            <button className='bg-[#FFE194] text-[#4C4C6D]  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-gray-500'>Log In</button>
+            <button className='bg-btn text-white  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-orange-700'>Sign Up</button>
+            <button className='bg-btn text-white  rounded-xl px-4 py-2 w-32 h-12 font-semibold hover:bg-orange-700'>Log In</button>
             </div>
             </div>
         </nav>
