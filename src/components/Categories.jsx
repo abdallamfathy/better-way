f69bfb2612ea714ab4ff361ca61bc085fec54e24
abdallamfathy <abdallamfathy@gmail.com>
2023-01-11 00:50:41 +0200

    edit categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,14 +6,14 @@ const Categories = () => {
   return (
     <div className='flex flex-col items-start gap-4 m-5 text-white'>
         <div>
-            <h1 className='text-2xl font-semibold mb-5 -ml-2'>OUR CATEGORIES</h1>
+            <h1 className='text-2xl font-bold mb-4 '>OUR CATEGORIES</h1>
         </div>
         <div className='w-full h-full'>
             <ul className='flex flex-col gap- w-full h-full'>
                 {categories.map((category) => (
                     <div className='flex items-center gap-4 hover:bg-slate-300 hover:text-black w-full  h-12 '>
-                    <li><AiOutlineCar/></li>
-                    <li className='text-lg font-normal' key={category.id}>{category.name}</li>
+                    <li className='font-medium text-lg'><AiOutlineCar/></li>
+                    <li className='text-lg font-medium' key={category.id}>{category.name}</li>
                     </div>
                 ))}
             </ul>
