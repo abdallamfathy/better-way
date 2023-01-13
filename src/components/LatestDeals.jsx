f62abb460bdb743ef7791a250335e3ab85d4e7ad
abdallamfathy <abdallamfathy@gmail.com>
2023-01-14 01:02:35 +0200

    make latest deals desktop

--- src/components/LatestDeals.jsx
+++ src/components/LatestDeals.jsx
@@ -16,11 +16,11 @@ import 'swiper/css/scrollbar';
 const LatestDeals = () => {
 return (
     <>
-    <div className='my-7'>
-    <div className='flex justify-center items-center  bg-bg py-1'>
-      <h2 className='text-txt  text-lg'>LATEST DEALS</h2>
+    <div className='my-7 md:my-20 md:mx-40'>
+    <div className='flex justify-center items-center  bg-bg py-1 md:h-20'>
+      <h2 className='text-txt  text-lg md:text-xl md:font-semibold'>LATEST DEALS</h2>
     </div>
-  <div className='mx-3 my-5 '>
+  <div className='mx-3 my-5 md:hidden '>
 
     <Swiper
       // install Swiper modules
@@ -136,6 +136,229 @@ return (
 </SwiperSlide>
     </Swiper>
     </div>
+
+
+    {/* desktop version */}
+  <div className='my-5 max-md:hidden'>
+
+<Swiper
+  // install Swiper modules
+modules={[Navigation, Pagination, Scrollbar, A11y]}
+spaceBetween={100}
+slidesPerView={7}
+>
+
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl">
+<div className="flex flex-col justify-center">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={zara} alt="product image" />
+</a>
+<div className="flex items-center justify-center gap-4">
+<div className="mx-2 mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">ZARA FOR MEN</h2>
+    <h3>WOMEN CLOTHES</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl shadow-white" >
+<div className="flex flex-col ">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={cafe} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2">
+<div className=" mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">CILANTRO CAFE</h2>
+    <h3>CAFE AND RESTAURANT</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl ">
+<div className="flex flex-col">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={tie} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2">
+<div className="mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">TIE HOUSE FOR MEN</h2>
+    <h3>MEN CLOTHES</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl shadow-2xl">
+<div className="flex flex-col">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={restaurant} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2 ">
+<div className="mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">SAIGON RESTAURANT</h2>
+    <h3>CAFE & RESTAURANT</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl">
+<div className="flex flex-col">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={zara} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2 ">
+<div className="mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">ZARA FOR MEN</h2>
+    <h3>WOMEN CLOTHES</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl shadow-white" >
+<div className="flex flex-col">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={cafe} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2 ">
+<div className="mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">CILANTRO CAFE</h2>
+    <h3>CAFE AND RESTAURANT</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl ">
+<div className="flex flex-col">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={tie} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2 ">
+<div className="mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">TIE HOUSE FOR MEN</h2>
+    <h3>MEN CLOTHES</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+<SwiperSlide>
+<div className="w-48 h-56 bg-white rounded-2xl shadow-2xl">
+<div className="flex flex-col">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={restaurant} alt="product image" />
+</a>
+<div className="flex items-center justify-between mx-2 ">
+<div className="mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">SAIGON RESTAURANT</h2>
+    <h3>CAFE & RESTAURANT</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute bottom-48 left-36 w-6">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+
+</Swiper>
+</div>
   </div>
 </>
 )
--- src/components/TopSales.jsx
+++ src/components/TopSales.jsx
@@ -20,122 +20,125 @@ return (
     <div className='flex justify-center items-center  bg-bg py-1 md:h-20'>
       <h2 className='text-txt  text-lg md:text-xl md:font-semibold'>TOP PLACES</h2>
     </div>
-  <div className='my-5 mx-3 md:hidden'>
+    <div className='mx-3 my-5 md:hidden '>
 
-    <Swiper
-      // install Swiper modules
-    modules={[Navigation, Pagination, Scrollbar, A11y]}
-    spaceBetween={20}
-    slidesPerView={2.5}
-    >
-    
-    <SwiperSlide>
-<div className="w-36 h-44 bg-white rounded-2xl">
-    <div className="flex flex-col justify-center">
-    <a href="#">
-        <img className="rounded-t-2xl w-48 h-36" src={zara} alt="product image" />
-    </a>
-    <div className="flex items-center gap-4">
-    <div className="mx-2 mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
-        <h2 className="text-btn">ZARA FOR MEN</h2>
-        <h3>WOMEN CLOTHES</h3>
-    </div>
-    <div className="flex w text-btn bg-bg rounded-md h-6  justify-center items-center">
-    <RiStarSFill className="w-20"/>
-    <RiStarSFill className="w-20"/>
-    <RiStarSFill className="w-20"/>
-    <RiStarSFill className="w-20"/>
-    <RiStarSLine className="w-20"/>
-    </div>
-    </div>
-    <div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
-        <h2 className="text-[8px] text-txt">11%</h2>
-    </div>
-    </div>
-    
-</div>
-</SwiperSlide>
-    <SwiperSlide>
-<div className="w-36 h-44 bg-white rounded-2xl shadow-white" >
-    <div className="flex flex-col justify-center">
-    <a href="#">
-        <img className="rounded-t-2xl w-48 h-36" src={cafe} alt="product image" />
-    </a>
-    <div className="flex items-center gap-4">
-    <div className="mx-2 mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
-        <h2 className="text-btn">CILANTRO CAFE</h2>
-        <h3>CAFE AND RESTAURANT</h3>
-    </div>
-    <div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSLine/>
-    </div>
-    </div>
-    <div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
-        <h2 className="text-[8px] text-txt">11%</h2>
-    </div>
-    </div>
-    
-</div>
-</SwiperSlide>
-    <SwiperSlide>
-<div className="w-36 h-44 bg-white rounded-2xl ">
-    <div className="flex flex-col justify-center">
-    <a href="#">
-        <img className="rounded-t-2xl w-48 h-36" src={tie} alt="product image" />
-    </a>
-    <div className="flex items-center gap-4">
-    <div className="mx-2 mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
-        <h2 className="text-btn">TIE HOUSE FOR MEN</h2>
-        <h3>MEN CLOTHES</h3>
-    </div>
-    <div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSLine/>
-    </div>
-    </div>
-    <div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
-        <h2 className="text-[8px] text-txt">11%</h2>
-    </div>
-    </div>
-    
-</div>
-</SwiperSlide>
-    <SwiperSlide>
+<Swiper
+  // install Swiper modules
+modules={[Navigation, Pagination, Scrollbar, A11y]}
+spaceBetween={20}
+slidesPerView={2.5}
+>
+<SwiperSlide>
 <div className="w-36 h-44 bg-white rounded-2xl shadow-2xl">
-    <div className="flex flex-col justify-center">
-    <a href="#">
-        <img className="rounded-t-2xl w-48 h-36" src={restaurant} alt="product image" />
-    </a>
-    <div className="flex items-center gap-4">
-    <div className="mx-2 mt-5  text-black text-[12px] font-semibold flex flex-col items-start justify-start">
-        <h2 className="text-btn">SAIGON RESTAURANT</h2>
-        <h3>CAFE & RESTAURANT</h3>
-    </div>
-    <div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSFill/>
-    <RiStarSLine/>
-    </div>
-    </div>
-    <div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
-        <h2 className="text-[8px] text-txt">11%</h2>
-    </div>
-    </div>
-    
+<div className="flex flex-col justify-center">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={restaurant} alt="product image" />
+</a>
+<div className="flex items-center gap-4">
+<div className="mx-2 my-1  text-black text-[6px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">SAIGON RESTAURANT</h2>
+    <h3>CAFE & RESTAURANT</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
 </div>
 </SwiperSlide>
-    
-    </Swiper>
-    </div>
+<SwiperSlide>
+<div className="w-36 h-44 bg-white rounded-2xl ">
+<div className="flex flex-col justify-center">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={tie} alt="product image" />
+</a>
+<div className="flex items-center gap-4">
+<div className="mx-2 my-1  text-black text-[6px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">TIE HOUSE FOR MEN</h2>
+    <h3>MEN CLOTHES</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+
+<SwiperSlide>
+<div className="w-36 h-44 bg-white rounded-2xl shadow-white" >
+<div className="flex flex-col justify-center">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={cafe} alt="product image" />
+</a>
+<div className="flex items-center gap-4">
+<div className="mx-2 my-1  text-black text-[6px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">CILANTRO CAFE</h2>
+    <h3>CAFE AND RESTAURANT</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1 bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+
+<SwiperSlide>
+<div className="w-36 h-44 bg-white rounded-2xl">
+<div className="flex flex-col justify-center">
+<a href="#">
+    <img className="rounded-t-2xl w-48 h-36" src={zara} alt="product image" />
+</a>
+<div className="flex items-center gap-4">
+<div className="mx-2 my-1  text-black text-[6px] font-semibold flex flex-col items-start justify-start">
+    <h2 className="text-btn">ZARA FOR MEN</h2>
+    <h3>WOMEN CLOTHES</h3>
+</div>
+<div className="flex w-10 text-btn bg-bg rounded-md h-3 px-1 justify-center items-center">
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSFill/>
+<RiStarSLine/>
+</div>
+</div>
+<div className="rounded-full p-1   bg-btn bg-opacity-80 border border-white absolute top-1 right-1">
+    <h2 className="text-[8px] text-txt">11%</h2>
+</div>
+</div>
+
+</div>
+</SwiperSlide>
+</Swiper>
+</div>
+
+
+    {/* desktop version */}
   <div className='my-5 max-md:hidden'>
 
     <Swiper
