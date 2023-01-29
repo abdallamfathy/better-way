c1044e73bbfd88a26a8b314d9768ef6b10fd231e
xdangos <abdallamfathy@gmail.com>
2023-01-29 15:06:14 +0200

    edit categories colors and size

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
-import { FaHospitalUser } from 'react-icons/fa'
+import { FaCaretSquareLeft, FaHospitalUser, FaMapMarkedAlt } from 'react-icons/fa'
 import { categories } from '../assets/Constants'
 
 const Categories = () => {
@@ -8,12 +8,52 @@ const Categories = () => {
     <div className='flex flex-col items-start gap-4 m-5 text-white'>
         <div className='w-full h-full'>
             <ul className='flex flex-col gap- w-full h-full'>
-                {categories.map((category) => (
-                    <div className='flex items-center gap-4 hover:bg-slate-300 hover:text-black w-full  h-12 '>
-                    <li className='font-medium text-lg'><FaHospitalUser/></li>
-                    <li className='text-[14px] font-medium' key={category.id}>{category.name}</li>
+                {/* {categories.map((category) => (
+                    <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' key={category.id}>{category.name}</li>
                     </div>
-                ))}
+                ))} */}
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaMapMarkedAlt/></li>
+                    <li className='text-lg font-medium' >Super Market</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaCaretSquareLeft/></li>
+                    <li className='text-lg font-medium' >Cafe</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Restaurant</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Cars</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Fashion</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Health Care</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Education</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Jewerly</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Net Cafe & Gaming</li>
+                    </div>
+                <div className='flex items-center gap-4 cursor-pointer  hover:text-btn w-full  h-12 '>
+                    <li className='font-medium text-xl'><FaHospitalUser/></li>
+                    <li className='text-lg font-medium' >Others</li>
+                    </div>
             </ul>
         </div>
     </div>
