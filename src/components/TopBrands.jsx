2e7f65dadc8c77f84b56ba53a401781b4d1b1b8f
xdangos <abdallamfathy@gmail.com>
2023-01-30 17:19:18 +0200

    top brands 4 brands

--- src/components/TopBrands.jsx
+++ src/components/TopBrands.jsx
@@ -13,28 +13,34 @@ const TopBrands = () => {
     <>
         <div className='my-7 md:my-12 md:mx-40 '>
         <div className='flex justify-center items-center  bg-bg py-1 md:h-20'>
-          <h2 className='text-txt  text-lg md:text-2xl md:font-semibold'>TOP BRANDS</h2>
+          <h2 className='text-txt  text-lg md:text-3xl md:font-semibold'>TOP BRANDS</h2>
         </div>
-      <div className='mx-3 my-5 md:m-10'>
+      <div className='mx-3 my-5 md:my-10 md:mx-0'>
 
-        <div className='flex flex-wrap  justify-center items-center gap-[1.68rem] md:gap-x-20 md:gap-10'>
+        <div className='flex flex-wrap  justify-between items-center gap-[1.68rem]  md:gap-10'>
           <div>
-            <img src={logo1} alt="logo" className='w-28 h-16 rounded-md md:w-96 md:h-36' />
+            <img src={logo1} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
           </div>
           <div>
-            <img src={logo2} alt="logo" className='w-28 h-16 rounded-md md:w-96 md:h-36' />
+            <img src={logo1} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
           </div>
           <div>
-            <img src={logo6} alt="logo" className='w-28 h-16 rounded-md md:w-96 md:h-36' />
+            <img src={logo2} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
           </div>
           <div>
-            <img src={logo5} alt="logo" className='w-28 h-16 rounded-md md:w-96 md:h-36' />
+            <img src={logo6} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
           </div>
           <div>
-            <img src={logo4} alt="logo" className='w-28 h-16 rounded-md md:w-96 md:h-36' />
+            <img src={logo5} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
           </div>
           <div>
-            <img src={logo3} alt="logo" className='w-28 h-16 rounded-md md:w-96 md:h-36' />
+            <img src={logo4} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
+          </div>
+          <div>
+            <img src={logo5} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
+          </div>
+          <div>
+            <img src={logo3} alt="logo" className='w-28 h-16 rounded-md md:w-64 md:h-36' />
           </div>
         </div>
         </div>
