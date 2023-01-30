2e0cf612cb1db0e3e97f0992f629bc555a178bdd
xdangos <abdallamfathy@gmail.com>
2023-01-30 18:02:09 +0200

    larger card in top places

--- src/components/TopBrands.jsx
+++ src/components/TopBrands.jsx
@@ -15,7 +15,7 @@ const TopBrands = () => {
         <div className='flex justify-center items-center  bg-bg py-1 md:h-20'>
           <h2 className='text-txt  text-lg md:text-3xl md:font-semibold'>TOP BRANDS</h2>
         </div>
-      <div className='mx-3 my-5 md:my-10 md:mx-0'>
+      <div className='mx-3 my-5 md:my-12 md:mx-0'>
 
         <div className='flex flex-wrap  justify-between items-center gap-[1.68rem]  md:gap-10'>
           <div>
