a1a4f6780bf95b4ec46d8f13beb7af74c5e4ed1e
abdallamfathy <abdallamfathy@gmail.com>
2023-02-13 20:55:24 +0200

    add rating filter to category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Footer, Navbar } from '../components'
 import zara from "../assets/topPlaces/zara.jpg";
+import { RiStarSFill } from 'react-icons/ri';
 
 
 const Category = () => {
@@ -8,16 +9,77 @@ const Category = () => {
     <>
         <Navbar/>
         <div className='mx-20 my-8'>
+            <div className='flex flex-col gap-6 text-txt'>
         <section className='sectionI'>
             <div className='w-full h-96'>
                 <img src={zara} alt="image" className='h-full w-full object-cover object-center'/>
             </div>
         </section>
         <section className='sectionII'>
-            <div>
-                
+            <div className='flex justify-between'>
+                <div className='w-1/4 bg-bg'>
+                    <div className='flex flex-col filter'>
+                            <div className='flex flex-col justify-center items-start p-4'>
+                                <h1 className='text-2xl font-bold'>PRODUCT RATING</h1>
+                                <table className='m-5 flex flex-col gap-2'>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-btn text-xl'>
+                                        <RiStarSFill />
+                                        <RiStarSFill />
+                                        <RiStarSFill />
+                                        <RiStarSFill />
+                                        <RiStarSFill className='text-txt' />
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-btn text-xl'>
+                                        <RiStarSFill />
+                                        <RiStarSFill />
+                                        <RiStarSFill />
+                                        <RiStarSFill className='text-txt'/>
+                                        <RiStarSFill className='text-txt'/>
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-btn text-xl'>
+                                        <RiStarSFill />
+                                        <RiStarSFill />
+                                        <RiStarSFill className='text-txt'/>
+                                        <RiStarSFill className='text-txt'/>
+                                        <RiStarSFill className='text-txt'/>
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-btn text-xl'>
+                                        <RiStarSFill />
+                                        <RiStarSFill className='text-txt'/>
+                                        <RiStarSFill className='text-txt'/>
+                                        <RiStarSFill className='text-txt'/>
+                                        <RiStarSFill className='text-txt'/>
+                                        </div>
+                                        </div>
+                                    </tr>
+                                </table>
+                            </div>
+                    </div>
+                </div>
+                <div className='w-3/4'>
+
+                </div>
             </div>
         </section>
+        </div>
         </div>
         <Footer/>
     </>
