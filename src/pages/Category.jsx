8abe216ec6886111912f3fa46e1bb79832fdd4f0
abdallamfathy <abdallamfathy@gmail.com>
2023-02-13 21:06:19 +0200

    make city & offer filter shop page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -17,9 +17,46 @@ const Category = () => {
         </section>
         <section className='sectionII'>
             <div className='flex justify-between'>
-                <div className='w-1/4 bg-bg'>
+                <div className='w-1/4 bg-bg  rounded-md'>
                     <div className='flex flex-col filter'>
-                            <div className='flex flex-col justify-center items-start p-4'>
+                            <div className='CITY flex flex-col justify-center items-start p-4 border-b border-txt'>
+                                <h1 className='text-2xl font-bold'>CITY</h1>
+                                <table className='m-5 flex flex-col gap-2'>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Cairo
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Alexandira
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Ismailia
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Suez
+                                        </div>
+                                        </div>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div className='RATING flex flex-col justify-center items-start p-4 border-b border-txt'>
                                 <h1 className='text-2xl font-bold'>PRODUCT RATING</h1>
                                 <table className='m-5 flex flex-col gap-2'>
                                     <tr>
@@ -67,6 +104,43 @@ const Category = () => {
                                         <RiStarSFill className='text-txt'/>
                                         <RiStarSFill className='text-txt'/>
                                         <RiStarSFill className='text-txt'/>
+                                        </div>
+                                        </div>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div className='OFFERS flex flex-col justify-center items-start p-4'>
+                                <h1 className='text-2xl font-bold'>OFFERS</h1>
+                                <table className='m-5 flex flex-col gap-2'>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            80% or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            60% or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            40% or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            20% or more
                                         </div>
                                         </div>
                                     </tr>
