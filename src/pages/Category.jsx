6e6fae45f25d53774888c72e86af650659343ff9
abdallamfathy <abdallamfathy@gmail.com>
2023-02-14 19:50:25 +0200

    add area and gained points filter

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -17,7 +17,7 @@ const Category = () => {
             </div>
         </section>
         <section className='sectionII'>
-            <div className='flex justify-between gap-4'>
+            <div className='flex justify-between'>
                 <div className='w-1/4 h-full bg-bg  rounded-md '>
                     <div className='flex flex-col filter'>
                             <div className='CITY flex flex-col justify-center items-start p-4 border-b border-txt'>
@@ -52,6 +52,43 @@ const Category = () => {
                                         <input type="checkbox" name="" id="" />
                                         <div className='flex justify-between text-xl'>
                                             Suez
+                                        </div>
+                                        </div>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div className='AREA flex flex-col justify-center items-start p-4 border-b border-txt'>
+                                <h1 className='text-2xl font-bold'>AREA</h1>
+                                <table className='m-5 flex flex-col gap-2'>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Misr El gededa
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Shubra
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Tagamo3
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="checkbox" name="" id="" />
+                                        <div className='flex justify-between text-xl'>
+                                            Zayed
                                         </div>
                                         </div>
                                     </tr>
@@ -147,11 +184,230 @@ const Category = () => {
                                     </tr>
                                 </table>
                             </div> 
+                            <div className='GAINED flex flex-col justify-center items-start p-4'>
+                                <h1 className='text-2xl font-bold'>GAINED</h1>
+                                <table className='m-5 flex flex-col gap-2'>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            10 or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            5 or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            4 or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                    <tr>
+                                        <div className='flex gap-4 items-center'>
+                                        <input type="radio" name="" id="" />
+                                        <div className='flex justify-between text-txt text-xl'>
+                                            2 or more
+                                        </div>
+                                        </div>
+                                    </tr>
+                                </table>
+                            </div> 
 
                     </div>
                 </div>
                 <div className='w-3/4'>
-                    <div className='flex flex-wrap gap-4'>
+                    <div className='flex  flex-wrap justify-end gap-4'>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
+                    <div className="w-60 h-72 bg-txt rounded-2xl">
+                      <div className="flex flex-col h-72">
+                        <div className="h-3/4">
+                          <img
+                            className="rounded-t-2xl w-full h-full"
+                            src={zara}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="flex items-center justify-center gap-4 h-1/4">
+                          <div className="mx-2    text-black text-[12px] font-semibold flex flex-col items-start justify-center">
+                            <h2 className="text-btn">ZARA FOR MEN</h2>
+                            <h3>WOMEN CLOTHES</h3>
+                          </div>
+                          <div className="flex w-20 text-btn bg-bg rounded-md h-3 p-2 justify-center items-center">
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        </div>
+                        <div className="rounded-full flex justify-center items-center bg-btn bg-opacity-80 border border-txt w-10 h-10 absolute  z-50 translate-x-44  translate-y-5">
+                          <h2 className="text-sm  text-txt text-center">11%</h2>
+                        </div>
+                      </div>
+                        </div>
                     <div className="w-60 h-72 bg-txt rounded-2xl">
                       <div className="flex flex-col h-72">
                         <div className="h-3/4">
