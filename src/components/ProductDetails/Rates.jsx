3be0f301c9cb078eadcc1e0d7260b488ddbaf949
abdallamfathy <abdallamfathy@gmail.com>
2023-04-29 18:06:23 +0200

    comments mob vers in shopPage

---
+++ src/components/ProductDetails/Rates.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Rates = () => {
+  return (
+    <div className="Rating">
+              <div className="SubTitle w-full "><h2 className="text-5xl font-semibold">Rating & Reviews :</h2></div>
+              <div className="Stars my-6 text-txt">
+
+                <div className="flex items-center mb-3">
+                  <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                  <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                  <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                  <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                  <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                  <p className="ml-2 text-sm font-medium ">4.95 out of 5</p>
+                </div>
+                <p className="text-sm font-medium">1,745 global ratings</p>
+                <div className="flex items-center mt-4">
+                  <span className="text-sm font-medium ">5 star</span>
+                  <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                    <div className="h-5 bg-btn rounded w-11/12" ></div>
+                  </div>
+                  <span className="text-sm font-medium ">90%</span>
+                </div>
+                <div className="flex items-center mt-4">
+                  <span className="text-sm font-medium ">4 star</span>
+                  <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                    <div className="h-5 bg-btn rounded w-4/5" ></div>
+                  </div>
+                  <span className="text-sm font-medium ">17%</span>
+                </div>
+                <div className="flex items-center mt-4">
+                  <span className="text-sm font-medium ">3 star</span>
+                  <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                    <div className="h-5 bg-btn rounded w-9/12" ></div>
+                  </div>
+                  <span className="text-sm font-medium ">8%</span>
+                </div>
+                <div className="flex items-center mt-4">
+                  <span className="text-sm font-medium ">2 star</span>
+                  <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                    <div className="h-5 bg-btn rounded w-20" ></div>
+                  </div>
+                  <span className="text-sm font-medium ">4%</span>
+                </div>
+                <div className="flex items-center mt-4">
+                  <span className="text-sm font-medium ">1 star</span>
+                  <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                    <div className="h-5 bg-btn rounded w-10" ></div>
+                  </div>
+                  <span className="text-sm font-medium ">1%</span>
+                </div>
+
+              </div>
+            </div> 
+  )
+}
+
+export default Rates
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -26,6 +26,8 @@ import { faArrowAltCircleRight, faArrowCircleRight, faArrowDown, faArrowDown19, 
 import { Rating } from "flowbite-react";
 import { ProductInfo } from "../components/ProductDetails/ProductInfo";
 import { Offers } from "../components/ProductDetails/Offers";
+import Comments from "../components/ProductDetails/Comments";
+import Rates from "../components/ProductDetails/Rates";
 
 const Shop = () => {
 
@@ -34,103 +36,24 @@ const Shop = () => {
       <Navbar />
       <div className="md:mx-20 md:my-8 m-5">
         <section className="sectionI">
-<ProductInfo   />
+          <ProductInfo />
         </section>
 
         <section className="sectionII my-4">
           <div className="Container flex justify-between gap-4">
             <div className="×OffersSection flex flex-col items-start  justify-center gap-4 h-full md:w-2/3   text-txt">
               <div className="Title w-full "><h2 className="md:text-6xl text-3xl font-bold text-btn ">Flamingo Cafe</h2></div>
-              <Offers     />
+              <Offers />
               {/* <div className="Description">
                 <div className="SubTitle w-full my-4"><h2 className="text-5xl font-semibold">Description :</h2></div>
                 <div className="leading-10 p-5 my-2">
                   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni sapiente veniam laborum eligendi cum iusto. Voluptate illo possimus, veritatis tempore molestias, omnis dolores laboriosam reiciendis doloribus ducimus culpa tempora voluptatum!</p>
                 </div>
               </div>
-              <div className="Rating">
-                <div className="SubTitle w-full "><h2 className="text-5xl font-semibold">Rating & Reviews :</h2></div>
-                <div className="Stars my-6 text-txt">
+            */}
+              <Rates/>
 
-                  <div className="flex items-center mb-3">
-                    <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 text-btn" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <p className="ml-2 text-sm font-medium ">4.95 out of 5</p>
-                  </div>
-                  <p className="text-sm font-medium">1,745 global ratings</p>
-                  <div className="flex items-center mt-4">
-                    <span className="text-sm font-medium ">5 star</span>
-                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-                      <div className="h-5 bg-btn rounded w-11/12" ></div>
-                    </div>
-                    <span className="text-sm font-medium ">90%</span>
-                  </div>
-                  <div className="flex items-center mt-4">
-                    <span className="text-sm font-medium ">4 star</span>
-                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-                      <div className="h-5 bg-btn rounded w-4/5" ></div>
-                    </div>
-                    <span className="text-sm font-medium ">17%</span>
-                  </div>
-                  <div className="flex items-center mt-4">
-                    <span className="text-sm font-medium ">3 star</span>
-                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-                      <div className="h-5 bg-btn rounded w-9/12" ></div>
-                    </div>
-                    <span className="text-sm font-medium ">8%</span>
-                  </div>
-                  <div className="flex items-center mt-4">
-                    <span className="text-sm font-medium ">2 star</span>
-                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-                      <div className="h-5 bg-btn rounded w-20" ></div>
-                    </div>
-                    <span className="text-sm font-medium ">4%</span>
-                  </div>
-                  <div className="flex items-center mt-4">
-                    <span className="text-sm font-medium ">1 star</span>
-                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
-                      <div className="h-5 bg-btn rounded w-10" ></div>
-                    </div>
-                    <span className="text-sm font-medium ">1%</span>
-                  </div>
-
-                </div>
-              </div> */}
-
-              <div className="Comments text-txt">
-
-                <article>
-                  <div className="flex items-center mb-4 space-x-4">
-                    <img className="w-10 h-10 rounded-full" src="https://avatars.githubusercontent.com/u/191027?v=4" alt="" />
-                    <div className="space-y-1 font-medium dark:text-white">
-                      <p>Mickel peter <time datetime="2023-01-16 19:00" className="block text-sm ">Joined on February 2023</time></p>
-                    </div>
-                  </div>
-                  <div className="flex items-center mb-1 text-btn">
-                    <svg aria-hidden="true" className="w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <svg aria-hidden="true" className="w-5 h-5 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <h3 className="ml-2 text-sm font-semibold text-txt">Thinking to buy another one!</h3>
-                  </div>
-                  <footer className="mb-5 text-sm "><p>Reviewed in Cairo , Egypt on <time datetime="2017-03-03 19:00">February 3, 2023</time></p></footer>
-                  <p className="mb-2 font-light ">This is my third time shopping from BetterWay. They are just fantastic value for money. This one arrived yesterday and the first thing I did was set the time, popped on an identical strap from another Invicta and went in the shower with it to test the waterproofing.... No problems.</p>
-                  <p className="mb-3 font-light ">It is obviously not the same build quality as those very expensive watches. But that is like comparing a Citroën to a Ferrari. This watch was well under 100EGP! An absolute bargain.</p>
-                  <a href="#" className="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Read more</a>
-                  {/* <aside>
-        <p className="mt-1 text-xs ">19 people found this helpful</p>
-        <div className="flex items-center mt-3 space-x-3 divide-x divide-gray-200 dark:divide-gray-600">
-            <a href="#" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-xs px-2 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Helpful</a>
-            <a href="#" className="pl-4 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Report abuse</a>
-        </div>
-    </aside> */}
-                </article>
-
-              </div>
+              <Comments/>
             </div>
 
 
