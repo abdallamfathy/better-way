a4adabd7f6a93913a765ba794c5400fd155b48a7
abdallamfathy <abdallamfathy@gmail.com>
2023-06-11 20:45:09 +0300

    fetch merchant papers from apis

--- src/components/MerchantPanel/MerchantPaper.jsx
+++ src/components/MerchantPanel/MerchantPaper.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import API_BASE_URL from "../../../config";
+import { useAuthUser } from 'react-auth-kit';
 
 const MerchantPaper = () => {
+  const [myData, setMyData] = useState(null);
+  const auth = useAuthUser();
+  const token = auth()?.token;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${API_BASE_URL}api/v1/merchants/papers` , 
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      }
+      );
+      const data = await response.json();
+      setMyData(data.data);
+      console.log(data.data);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div>MerchantPaper</div>
+    <div className='flex flex-wrap'>
+      {
+        myData?.map((item, index) => (
+          <div key={index} className='w-full md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5 p-2'>
+                <img className='w-96 h-96 rounded-full' src={item.logo} alt='logo' />
+                </div>
+        ))
+      }
+    </div>
   )
 }
 
