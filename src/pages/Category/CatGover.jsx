eb4ebdc0af5a7ca8df0793197f3069cbc2125345
abdallamfathy <abdallamfathy@gmail.com>
2023-09-09 15:01:02 +0300

    fix filters 2

--- src/pages/Category/CatGover.jsx
+++ src/pages/Category/CatGover.jsx
@@ -2,49 +2,63 @@ import React, { useEffect, useState } from 'react'
 import API_BASE_URL from '../../../config';
 import { useLangContext } from '../../utils/LangContext';
 
-const CatGover = ({setForm}) => {
-    const [myData , setMyData] = useState(null)
-    useEffect(() => {
-        const fetchData = async () => {
-          const response = await fetch(`${API_BASE_URL}api/v1/governorates`);
-          const data = await response.json();
-          setMyData(data.data);
-        };
-    
-        fetchData();
-      }, []);
-    //   const [selectedItem, setSelectedItem] = useState(null);
+const CatGover = ({ setForm }) => {
+  const [myData, setMyData] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${API_BASE_URL}api/v1/governorates`);
+      const data = await response.json();
+      setMyData(data.data);
+    };
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setForm((prevForm) => ({
-          ...prevForm,
-          [name]: value,
-        }));
-      };
-      const { language } = useLangContext();
+    fetchData();
+  }, []);
+  //   const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+  const { language } = useLangContext();
   return (
     <>
-        {
-            myData?.map((item) => {
-                return (
-                    <div className="flex gap-4 items-center" key={item?.id}>
-                        <input
-                        type="radio"
-                        name="governorate_id"
-                        id={item?.id}
-                        value={item?.id}
-                        onChange={handleChange}
-                        />
-                        <div className="flex justify-between 2xl:text-2xl xl:text-xl lg:text-lg">
-                            {
-                                language ? item?.title : item?.title_ar
-                            }
-                        </div>
-                    </div>
-                );
-            }
-)}
+      <div className="flex gap-4 items-center">
+        <input
+          type="radio"
+          name="governorate_id"
+          id={0}
+          value={''}
+          onChange={handleChange}
+        />
+        <div className="flex justify-between 2xl:text-2xl xl:text-xl lg:text-lg">
+          {
+            language ? "All" : "الكل"
+          }
+        </div>
+      </div>
+      {
+        myData?.map((item) => {
+          return (
+            <div className="flex gap-4 items-center" key={item?.id}>
+              <input
+                type="radio"
+                name="governorate_id"
+                id={item?.id}
+                value={item?.id}
+                onChange={handleChange}
+              />
+              <div className="flex justify-between 2xl:text-2xl xl:text-xl lg:text-lg">
+                {
+                  language ? item?.title : item?.title_ar
+                }
+              </div>
+            </div>
+          );
+        }
+        )}
     </>
   )
 }
--- src/pages/Category/Filter.jsx
+++ src/pages/Category/Filter.jsx
@@ -10,50 +10,50 @@ import { useLangContext } from "../../utils/LangContext";
 
 export function Filter({ setMyData }) {
 
-    const {categoryId} = useParams();
-    const [form , setForm] = useState({
-        search : "",
-        category : "",
-        governorate_id : 0,
-        area_id : 0,
-        discount_min : 1,
-        discount_max : 100,
-        discount_type : "fixed",
+    const { categoryId } = useParams();
+    const [form, setForm] = useState({
+        search: "",
+        category: "",
+        governorate_id: null,
+        area_id: null,
+        discount_min: null,
+        discount_max: null,
+        discount_type: null,
         gained_points_min: 1,
         gained_points_max: 5,
-        rate: 0
-         
+        rate: 1
+
     })
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(form);
         const res = await axios.get(`https://betterway.balkosolar.de/api/v1/categories/${categoryId}/filter`, {
-            params : form
+            params: form
         })
         console.log(res);
         setMyData(res?.data?.data);
-      };
+    };
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setForm((prevForm) => ({
-          ...prevForm,
-          [name]: parseInt(value, 10),
+            ...prevForm,
+            [name]: parseInt(value, 10),
         }));
-      };
+    };
     const handleChangeType = (event) => {
         const { name, value } = event.target;
         setForm((prevForm) => ({
-          ...prevForm,
-          [name]: value,
+            ...prevForm,
+            [name]: value,
         }));
-      };
-      const { language } = useLangContext();
-      const lang = language === true ? 'ltr' : 'rtl';
+    };
+    const { language } = useLangContext();
+    const lang = language === true ? 'ltr' : 'rtl';
 
-      return <form dir={lang} onSubmit={handleSubmit} className="flex flex-col filter w-full">
-         
+    return <form dir={lang} onSubmit={handleSubmit} className="flex flex-col filter w-full">
+
         <div className="CITY flex flex-col justify-center items-start p-4 border-b border-txt h-80  ">
             <h1 className="2xl:text-3xl xl:text-2xl text-xl font-bold">
                 {
@@ -61,7 +61,7 @@ export function Filter({ setMyData }) {
                 }
             </h1>
             <div className="p-5 flex flex-col gap-2 h-full w-full  overflow-y-auto">
-                <CatGover setForm={setForm}/> 
+                <CatGover setForm={setForm} />
             </div>
         </div>
         <div className="AREA flex flex-col justify-center items-start p-4 border-b border-txt h-80">
@@ -71,7 +71,7 @@ export function Filter({ setMyData }) {
                 }
             </h1>
             <div className="p-5 flex flex-col gap-2 w-full h-full overflow-y-auto">
-                <CatArea setForm={setForm}/>
+                <CatArea setForm={setForm} />
             </div>
         </div>
         <div className="DISCOUNT flex flex-col justify-center items-start p-4 border-b border-txt">
@@ -82,7 +82,7 @@ export function Filter({ setMyData }) {
             </h1>
             <div className="p-5 flex flex-col gap-2">
                 <div className="flex gap-2 items-center">
-                <input type="radio" name="discount_type" id="2" value={"percentage"} onChange={handleChangeType}/>
+                    <input type="radio" name="discount_type" id="2" value={"percentage"} onChange={handleChangeType} />
                     <p>
                         {
                             language ? "From" : "من"
@@ -98,7 +98,7 @@ export function Filter({ setMyData }) {
                     <p className="text-xl font-bold">%</p>
                 </div>
                 <div className="flex gap-2 items-center">
-                <input type="radio" name="discount_type" id="22" value={"fixed"} onChange={handleChangeType}/>
+                    <input type="radio" name="discount_type" id="22" value={"fixed"} onChange={handleChangeType} />
                     <p>
                         {
                             language ? "From" : "من"
@@ -115,8 +115,8 @@ export function Filter({ setMyData }) {
                         {
                             language ? "EGP" : "جنيه"
                         }
-                        
-                        </p>{" "}
+
+                    </p>{" "}
                 </div>
             </div>
         </div>
@@ -133,26 +133,26 @@ export function Filter({ setMyData }) {
                     }
                 </p>
                 <input
-                        type="number"
-                        name="gained_points_min"
-                        id={3}
-                        value={form.gained_points_min}
-                        onChange={handleChange}
-                        placeholder="0" 
-                        className="2xl:w-20 xl:w-16 lg:w-16 text-black font-semibold text-sm" />
+                    type="number"
+                    name="gained_points_min"
+                    id={3}
+                    value={form.gained_points_min}
+                    onChange={handleChange}
+                    placeholder="0"
+                    className="2xl:w-20 xl:w-16 lg:w-16 text-black font-semibold text-sm" />
                 <p>
                     {
                         language ? "To" : "إلى"
                     }
                 </p>
                 <input
-                        type="number"
-                        name="gained_points_max"
-                        id={4}
-                        value={form.gained_points_max}
-                        onChange={handleChange}
-                        placeholder="100"
-                        className="2xl:w-20 xl:w-16 lg:w-16 text-black font-semibold text-sm" />{" "}
+                    type="number"
+                    name="gained_points_max"
+                    id={4}
+                    value={form.gained_points_max}
+                    onChange={handleChange}
+                    placeholder="100"
+                    className="2xl:w-20 xl:w-16 lg:w-16 text-black font-semibold text-sm" />{" "}
             </div>
         </div>
         <div className="RATING flex flex-col justify-center items-start p-4 border-t border-txt">
@@ -161,12 +161,33 @@ export function Filter({ setMyData }) {
                     language ? "RATING & REVIEWS" : " التقييم والمراجعات "
                 }
             </h1>
-                <Rate setForm={setForm} form={form}/>
+            <Rate setForm={setForm} form={form} />
         </div>
+        <div className="flex items-center justify-between">
         <button type="submit" className="flex justify-start  m-10 my-5 bg-btn w-fit p-4 py-1 rounded-md">
             {
                 language ? "Submit" : "إرسال"
             }
         </button>
+        <button type="reset" onClick={() => setForm(
+            {
+                search: "",
+                category: "",
+                governorate_id: null,
+                area_id: null,
+                discount_min: null,
+                discount_max: null,
+                discount_type: null,
+                gained_points_min: 1,
+                gained_points_max: 5,
+                rate: 1
+
+            }
+        )} className="flex justify-start  m-10 my-5 bg-btn w-fit p-4 py-1 rounded-md">
+            {
+                language ? "Reset" : "إعادة تعيين"
+            }
+        </button>
+        </div>
     </form>;
 }
